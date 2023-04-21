1d231a3b0b019690aeaca9043b9128cac12e2ad7
Petiok <129048882+Petiok@github.com>
2023-04-20 20:37:21 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-
+#include <stdio.h>
+/**
+ * main -A program that prints line with puts function
+ * Return (0)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

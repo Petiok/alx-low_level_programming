a139490ad666b500ca426ecd0a145b21f4a535aa
Petiok <129048882+Petiok@github.com>
2023-04-22 21:46:54 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  *main -Entry point
  *
- *Return: Always (0)
+ *Return: (1)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2-15-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-return : 0 (failure)
+write(1, "and that piece of art is useful\" - Dora Korpar, 2-15-10-19\n", 59);
+return (1);
 }

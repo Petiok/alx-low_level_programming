1e87538356ec814cde1d097de74a6cc37c795dcb
Petiok <129048882+Petiok@github.com>
2023-04-27 07:37:14 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,18 +3,16 @@
 
 /**
  * Main - Test for positive or negative
- * Reurn: 0
+ * Return: 0
  */
 
 int main(void)
-
 {
-	int i;
+int i;
 
-	i = 0;
-
-	positive _or_negative (i);
+i = 0;
+positive _or_negative (i);
 	
-	return 0
+return (0);
 
 }

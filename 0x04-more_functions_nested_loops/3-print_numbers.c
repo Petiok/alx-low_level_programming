d45127f42f1951c681a408d848f4b405bd652810
Petiok <129048882+Petiok@github.com>
2023-04-27 05:40:53 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * print_number - Prints number since 0 to 9
+ * Return: 0
+  */
+
+void print_number(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; x++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,5 @@
+#include "main.h"
+
+
+/**
+ * print_most_numbers - Print the numbers since 0 to 9

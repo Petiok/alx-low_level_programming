6d4df2e099c1986d0550fbee8f7f1a3fb7b56b20
Petiok <129048882+Petiok@github.com>
2023-04-27 00:25:34 -0700

    0x03-debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * Main - Test for positive or negative
+ * Reurn: 0
+ */
+
+int main(void)
+
+{
+	int i;
+
+	i = 0;
+
+	positive _or_negative (i);
+	
+	return 0
+
+}
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Determine if the number is positive, negative, or zero
+ * 0 : Is the number to be checked
+ * Return: 0 (success)
+ */
+
+void positive_or_negative(int i)
+
+{
+if (1 < 0)
+{printf("%d is %s\n", 1, "negative");
+}
+else if (1 > 0)
+{
+printf("%d is %s\n", 1, "positive");
+}
+else
+{
+	printf("%d is %s\n", 1 "zero");
+}
+return (0);
+
+}

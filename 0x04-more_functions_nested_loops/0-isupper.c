4c08bf2ff42d5a318d635834f6483d1da602406b
Petiok <129048882+Petiok@github.com>
2023-04-27 02:10:29 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,14 @@
 
 /**
  * _isupper - Check is a letter is upper
- * @x: The number to be checked
+ * @c: The number to be checked
  * Return: 1 for upper and 0 for otherwise
  *
  */
 
-int _isupper(int x)
+int _isupper(int c)
 {
-	if (x >= 65 && x <- 90)
+	if (c >= 65 && c <- 90)
 	{
 	return (1);
 	}

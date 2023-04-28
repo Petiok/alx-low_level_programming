5c6bfba3689f406c6362ae425215d86c5fc8b101
Petiok <129048882+Petiok@github.com>
2023-04-28 04:39:31 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/**i
+/**
 * main - causes an infinite loop
 * Return: 0
 */
@@ -8,9 +8,9 @@
 int main(void)
 {
 	int i;
-	
+
 	printf("Infinite loop incoming :(\n");
-	
+
 	i = 0;
 	/**
 	 * while (i < 10)
